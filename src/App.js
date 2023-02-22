5689731714ff590f954e3705828aa8f2340054fa
김동현 <dtc03003@naver.com>
2023-02-23 00:13:46 +0900

    라이프사이클 연습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { Component } from 'react';
-import IterationSampe from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      <IterationSampe/>
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color}/>
+      </div>
     )
   }
 }
